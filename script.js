17048f58a60b0c211633530ea116f44363e82ebe
Saksham Kaushik <sakshamkauhsik3047@gmail.com>
2024-07-16 00:57:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+class CanvasPaint{
+    constructor(){
+        this.name = "Saksham";
+        this.age = 20;
+        this.data = data;
+}
+
+printDetails(){
+    console.log(this.age, this.name);
+}
+}
+
+let canvas = new CanvasPaint(12);
+canvas.printDetails();
